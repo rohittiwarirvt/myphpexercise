813fc6e197a4fd6bd4ff10899a6e5552cdb158f7
Rohit Tiwari <rohit.tiwari@webnosiselab.com>
2016-12-06 15:47:27 +0530

    Simple connection application to googel drive

--- google-connect/index.php
+++ google-connect/index.php
@@ -2,3 +2,98 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv->load();
+
+
+define('SCOPES', implode(' ', array(
+  Google_Service_Drive::DRIVE_METADATA_READONLY)
+));
+
+
+if (php_sapi_name() != 'cli') {
+  throw new Exception('This application must be run on the command line.');
+}
+
+/**
+ * Returns an authorized API client.
+ * @return Google_Client the authorized client object
+ */
+
+function getClient() {
+  $application_name = getenv('APPLICATION_NAME');
+  $client_secret_path = getenv('CLIENT_SECRET_PATH');
+  $credentials_path = getenv('CREDENTIALS_PATH');
+  $client = new Google_Client();
+  $client->setApplicationName($application_name);
+  $client->setScopes(SCOPES);
+  $client->setAuthConfig($client_secret_path);
+  $client->setAccessType('offline');
+
+
+// Load previously authorized credentials from a file.
+  $credentialsPath = expandHomeDirectory($credentials_path);
+if (file_exists($credentialsPath)) {
+    $accessToken = json_decode(file_get_contents($credentialsPath), true);
+  } else {
+    $authUrl = $client->createAuthUrl();
+    printf("Open the following link in your browser:\n%s\n", $authUrl);
+    print 'Enter verification code: ';
+    $authCode = trim(fgets(STDIN));
+    // Exchange authorization code for an access token.
+    $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+
+    // Store the credentials to disk.
+    if(!file_exists(dirname($credentialsPath))) {
+      mkdir(dirname($credentialsPath), 0700, true);
+    }
+
+    file_put_contents($credentialsPath, json_encode($accessToken));
+    printf("Credentials saved to %s\n", $credentialsPath);
+  }
+
+  $client->setAccessToken($accessToken);
+// Refresh the token if it's expired.
+  if ($client->isAccessTokenExpired()) {
+    $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+    file_put_contents($credentialsPath, json_encode($client->getAccessToken()));
+  }
+  return $client;
+}
+
+/**
+ * Expands the home directory alias '~' to the full path.
+ * @param string $path the path to expand.
+ * @return string the expanded path.
+ */
+function expandHomeDirectory($path) {
+  $homeDirectory = getenv('HOME');
+  if (empty($homeDirectory)) {
+    $homeDirectory = getenv('HOMEDRIVE') . getenv('HOMEPATH');
+  }
+  return str_replace('~', realpath($homeDirectory), $path);
+}
+
+
+// Get the API client and construct the service object.
+$client = getClient();
+$service = new Google_Service_Drive($client);
+
+
+// Print the names and IDs for up to 10 files.
+$optParams = array(
+  'pageSize' => 10,
+  'fields' => 'nextPageToken, files(id, name)'
+);
+
+$results = $service->files->listFiles($optParams);
+
+
+if (count($results->getFiles()) == 0) {
+  print "No files found.\n";
+} else {
+  print "Files:\n";
+  foreach ($results->getFiles() as $file) {
+    printf("%s (%s)\n", $file->getName(), $file->getId());
+  }
+}
