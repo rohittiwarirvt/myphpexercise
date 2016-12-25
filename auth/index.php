c650e6adf384e0dc0c9c1e11ffc5d9732a1db222
Rohit Tiwari <rohittiwarirvt@gmail.com>
2016-12-25 10:16:49 +0530

    added index.php

--- auth/index.php
+++ auth/index.php
@@ -1,3 +1,9 @@
 <?php
 
-require_once __DIR__ ."vendor/autoload.php";
+require_once __DIR__ ."/vendor/autoload.php";
+
+// phase one login and registration
+
+require_once __DIR__ . "/src/Views/phase1/header.php";
+
+require_once __DIR__ . "/src/Views/phase1/login.php";
