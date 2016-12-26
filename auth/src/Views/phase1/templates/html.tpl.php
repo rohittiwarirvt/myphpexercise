dc72cf2d170ada40a6ec278aac11170635558367
Rohit Tiwari <rohit.tiwari@webnosiselab.com>
2016-12-26 14:27:33 +0530

    added and made templates in html

---
+++ auth/src/Views/phase1/templates/html.tpl.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title><?php print $title; ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php print $styles; ?>
+    <?php print $scripts; ?>
+  </head>
+  <header id="header">
+    <?php print $header; ?>
+  </header>
+  <body class="container">
+    <?php print $content; ?>
+  </body>
+  <footer id="footer">
+    <?php print $footer; ?>
+  </footer>
+</html>
+
---
+++ auth/src/Views/phase1/templates/script.tpl.php
@@ -0,0 +1,1 @@
+<script type="text/javascript" src="/src/Assets/" . <?php print $js_path; ?>></script>
