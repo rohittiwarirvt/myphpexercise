ad17cdd2bcdde7655e2fcc5a4b21235d672425b3
Rohit Tiwari <rohittiwarirvt@gmail.com>
2016-12-26 08:42:38 +0530

    modification of index.php and route.php

--- auth/index.php
+++ auth/index.php
@@ -4,6 +4,18 @@ require_once __DIR__ ."/vendor/autoload.php";
 
 // phase one login and registration
 
+use RohitAuth\Routes\RegisterRoutes;
+use RohitAuth\Routes\Http\Request;
+use RohitAuth\Routes\Http\Routes;
+
+$request = new Request();
+$route = new Routes($request);
+$routes = new RegisterRoutes($route);
+
+
 require_once __DIR__ . "/src/Views/phase1/header.php";
 
-require_once __DIR__ . "/src/Views/phase1/login.php";
+require_once __DIR__ . "/src/Views/phase1/body.php";
+
+require_once __DIR__ . "/src/Views/phase1/footer.php";
+print $routes->initRoute();
