96b2d8ed299f7e5f4b20ee8ccd09d4cc437cec25
Rohit Tiwari <rohit.tiwari@webnosiselab.com>
2016-12-26 19:27:12 +0530

    modified index.php

--- auth/index.php
+++ auth/index.php
@@ -1,12 +1,16 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 require_once __DIR__ ."/vendor/autoload.php";
 
 // phase one login and registration
 
-use RohitAuth\Routes\RegisterRoutes;
 use RohitAuth\Routes\Http\Request;
 use RohitAuth\Routes\Http\Routes;
+use RohitAuth\Routes\RegisterRoutes;
+
 
 $request = new Request();
 $route = new Routes($request);
